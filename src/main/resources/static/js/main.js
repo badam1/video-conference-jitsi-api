b4ffa20c4953e01a72ef371f51f25baaebf7c075
bodansky-apertus <adam.bodansky@gmail.com>
2017-06-14 09:38:07 +0200

    - try

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,13 +6,17 @@
 
 $(function () {
 
-    var domain = "adam.progmasters.net";
+    var domain = "meet.jitsi.si";
     var room = $("#room");
     var width = 480;
     var height = 640;
     var htmlElement = document.querySelector("#meet");
+    var configOverwrite = {};
+    var interfaceConfigOverwrite = {
+        filmStripOnly: true
+    };
     var noSsl = false;
-    var api = new JitsiMeetExternalAPI(domain, room, width, height, htmlElement, undefined, undefined, noSsl);
+    var api = new JitsiMeetExternalAPI(domain, room, width, height, htmlElement, configOverwrite, interfaceConfigOverwrite, noSsl);
 
 });
 
